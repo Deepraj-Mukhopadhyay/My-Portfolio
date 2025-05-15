910bf6d26f1c5f1d8112a22ca16594062b35b78e
Deepraj Mukhopadhyay <143916300+Deepraj-Mukhopadhyay@users.noreply.github.com>
2025-05-15 13:56:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const toggleButton = document.getElementById("toggle-mode");
 
 toggleButton.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
+  document.body.classList.toggle("dark");
 
-  toggleButton.textContent = document.body.classList.contains("dark-mode")
+  toggleButton.textContent = document.body.classList.contains("dark")
     ? "Light Mode"
     : "Dark Mode";
 });
